bb447e596059adc47d745b8cec59daef5d3b8116
Michaelojok11 <michaelocan11@gmail.com>
2023-08-31 01:23:24 +0300

    update git

--- personal-portfolio-Ojok-Michael-Ocan/script.js
+++ personal-portfolio-Ojok-Michael-Ocan/script.js
@@ -31,3 +31,63 @@ servicesButtons.forEach(item => {
 getService('frontend');
 
 // End of services
+
+
+
+// mixitup portfolio
+
+const containerEl = document.querySelector('.projects_container');
+var mixer = mixitup(containerEl, {
+    // disable the animation
+    animation: {
+        enable:false
+    }
+});
+
+mixer.filter('*');
+
+
+
+
+//========== what people say / Swiper == testimonials==
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween:30,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true
+    },
+    breakpoints: {
+        600:{
+            slidesPerView:2,
+        },
+        1024: {
+            slidesPerView:3,
+        }
+    }
+
+  });
+
+//   snipet code
+document.addEventListener('DOMContentLoaded', function() {
+    const showMoreButton = document.getElementById('showMoreButton');
+    const projectContainer = document.getElementById('projectContainer');
+    const allProjects = projectContainer.querySelectorAll('.project');
+    const initialProjectCount = 6;
+
+    // Show only the first 6 projects initially
+    for (let i = 0; i < allProjects.length; i++) {
+        if (i < initialProjectCount) {
+            allProjects[i].style.display = 'block';
+        } else {
+            allProjects[i].style.display = 'none';
+        }
+    }
+
+    // Show more projects when the "Show More" button is clicked
+    showMoreButton.addEventListener('click', function() {
+        allProjects.forEach(project => project.style.display = 'block');
+        showMoreButton.style.display = 'none';
+    });
+});
