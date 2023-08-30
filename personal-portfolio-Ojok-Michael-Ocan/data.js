db12a5234fde0c51b5a0da0b5b82b57f42590aff
Michaelojok11 <michaelocan11@gmail.com>
2023-08-30 04:27:11 +0300

    update git

--- personal-portfolio-Ojok-Michael-Ocan/data.js
+++ personal-portfolio-Ojok-Michael-Ocan/data.js
@@ -23,13 +23,18 @@ const servicesData = [
     {
         title: 'GIS analyst/Developer',
         category: 'gis',
-        description: [`I specialize in front-end development, using HTML, CSS, and JavaScript to bring design concepts to life.
+        description: [` <p> I specialize in front-end development, using HTML, CSS, and JavaScript to bring design concepts to life.
             My focus on user experience means I prioritize aesthetics and seamless functionality. I excel in optimizing 
             performance, ensuring compatibility, and integrating APIs for enhanced user satisfaction. As a front-end developer, 
-            I transform designs into interactive web experiences with HTML, CSS, and JavaScript. 
+            I transform designs into interactive web experiences with HTML, CSS, and JavaScript.</p>
+            <p>
             I prioritize visual appeal and user-friendly functionality, implementing animations for engagement. 
             I am skilled in optimizing performance, ensuring cross-browser compatibility, and integrating APIs. 
-            My responsive designs guarantee consistent experiences on all devices.`]
+            My responsive designs guarantee consistent experiences on all devices.</p>
+            <p>
+            I prioritize visual appeal and user-friendly functionality, implementing animations for engagement. 
+            I am skilled in optimizing performance, ensuring cross-browser compatibility, and integrating APIs. 
+            My responsive designs guarantee consistent experiences on all devices.</p>`]
     },
     {
         title: 'Research',
--- personal-portfolio-Ojok-Michael-Ocan/script.js
+++ personal-portfolio-Ojok-Michael-Ocan/script.js
@@ -6,14 +6,26 @@ const servicesDetails = document.querySelector('.services_right');
 // function to get the service
 const getService = (category) => {
     const details = servicesData.find(item => item.category === category);
-    console.log(details);
+    servicesDetails.innerHTML = `
+    <h3>${details.title}</h3>
+    ${details.description.map(paragraph => "<p>" + paragraph + "</p>").join('')}`
 }
+
+const removeActiveClass = () =>  {
+    servicesButtons.forEach(button => {
+       button.classList.remove('active');
+        })
+    };
+
+
 
 
 // Add an event listener
 servicesButtons.forEach(item => {
     item.addEventListener('click', () => {
+        removeActiveClass();
         const serviceClass = item.classList[1];
-        getService(serviceClass)        
+        getService(serviceClass);
+        item.classList.add('active');        
     })
 })
