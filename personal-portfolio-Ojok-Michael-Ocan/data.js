b0928e2b431298d70915e9ef72ecbccced39590e
Michaelojok11 <michaelocan11@gmail.com>
2023-09-01 16:32:46 +0300

    update git

--- personal-portfolio-Ojok-Michael-Ocan/data.js
+++ personal-portfolio-Ojok-Michael-Ocan/data.js
@@ -2,12 +2,17 @@ const servicesData = [
     {
         title: 'Frontend Development',
         category: 'frontend',
-        description: [`My focus on user experience means I prioritize aesthetics and seamless functionality. I excel in optimizing 
-            performance, ensuring compatibility, and integrating APIs for enhanced user satisfaction. As a front-end developer, 
-            I transform designs into interactive web experiences with HTML, CSS, and JavaScript. 
-            I prioritize visual appeal and user-friendly functionality, implementing animations for engagement. 
-            I am skilled in optimizing performance, ensuring cross-browser compatibility, and integrating APIs. 
-            My responsive designs guarantee consistent experiences on all devices.`]
+        description: [`As a frontend developer, I craft visually captivating, responsive and highly 
+        functional user interfaces for websites and web applications. I bring your digital visions 
+        to life, ensuring seamless and engaging user experiences.
+        <br><br>
+        With my skills , I can contribute to your business / company's success by delivering captivating 
+        web experiences that engage customers, boost brand recognition, and drive business growth.
+        I am dedicated to helping your business / company thrive in the digital landscape and complete the tasks 
+        as assigned.
+            <br> <br>
+            <strong> Key skills</strong>
+            - HTML, CSS, Javascript, Bootstrap, react`]
     },
     {
         title: 'UX/UI Design',
@@ -23,18 +28,20 @@ const servicesData = [
     {
         title: 'GIS analyst/Developer',
         category: 'gis',
-        description: [` <p> I specialize in front-end development, using HTML, CSS, and JavaScript to bring design concepts to life.
-            My focus on user experience means I prioritize aesthetics and seamless functionality. I excel in optimizing 
-            performance, ensuring compatibility, and integrating APIs for enhanced user satisfaction. As a front-end developer, 
-            I transform designs into interactive web experiences with HTML, CSS, and JavaScript.</p>
+        description: [` <p>   With my Geographical Information Systems knowledge, I bring a potent blend of spatial
+         expertise and technical acumen.  I excel at  deciphering spatial data to derive valuable insights, 
+         providing mapping solutions, and facilitating data-driven decision-making across various industries. </p>
+        <p>
+         I also craft customized geospatial applications. My unique edge lies in merging GIS prowess with frontend 
+         development skills to create user-friendly, data-rich web applications, enhancing data accessibility, and 
+         improving overall service delivery. This fusion of skills allows me to carve out a distinctive niche, 
+         enabling organizations to excel in spatial data management and presentation, ultimately setting them apart 
+         in the competitive landscape.</p> 
             <p>
-            I prioritize visual appeal and user-friendly functionality, implementing animations for engagement. 
-            I am skilled in optimizing performance, ensuring cross-browser compatibility, and integrating APIs. 
-            My responsive designs guarantee consistent experiences on all devices.</p>
-            <p>
-            I prioritize visual appeal and user-friendly functionality, implementing animations for engagement. 
-            I am skilled in optimizing performance, ensuring cross-browser compatibility, and integrating APIs. 
-            My responsive designs guarantee consistent experiences on all devices.</p>`]
+            <strong> Skills: </strong>
+            <br>
+            ArcGis, ArcPy, QGIS, leaflet, Mapbox, OpenLayers, Python, HTML, CSS, Javascript.
+            .</p> `]
     },
     {
         title: 'Research',
